b75484f7c8c3bf5a607321c4a263979537628c85
leeewl <leeewl@163.com>
2023-03-09 21:21:02 +0800

    countNumOfCode

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# mytools
+# 这里收纳了自己写的工具
+
+## countNumOfCode
+统计了每天在git上提交的代码行数，是一个shell脚本。
+```shell
+sh main.sh 天数
+```
+
+
+## other
--- countNumOfCode/main.sh
+++ countNumOfCode/main.sh
@@ -1,4 +1,55 @@
 # 统计每日提交到git的代码行数
 
-a=`find ~/ -type d -name ".git"`
-echo $a
+# 输入的参数，天数
+if [ "$1" ];
+then
+	days=$1
+else
+	echo "需要参数，参数为数字，代表几日的数据\n"
+	exit
+fi
+
+#清空目录文件
+> ./gitDirectory.txt
+#查找玩家home目录下所有的.git目录，并根据关键字忽略部分目录
+for line in `find ~/ -type d -name ".git"`
+do
+	ignore=false
+	for ignoreWord in `cat ./ignoreKeyword.txt`
+	do
+		dir=`echo $line | grep -v $ignoreWord`
+		if [ -z $dir ]
+		then
+			ignore=true
+			break
+		fi
+	done;
+	if [ $ignore = false ]
+	then
+		#echo ${line::-4} >> ./gitDirectory.txt
+		echo $line >> ./gitDirectory.txt
+	fi
+done;
+
+zeroClock="00:00:00"
+for ((day=0;day<$days;day++));do 
+	dayBegin=$(date -d "$day days ago" "+%Y-%m-%d")
+	timeBegin="$dayBegin $zeroClock"
+	dayEnd=$(date -d "$((day-1)) days ago" "+%Y-%m-%d")
+	timeEnd="$dayEnd $zeroClock"
+
+	# 当日总数
+	dayTotal=0
+
+	# 进入每个git目录，计算出总行数
+	for gitDir in `cat ./gitDirectory.txt`
+	do
+		# 当日当文件夹总数
+		dirTotal=`git --git-dir $gitDir log --since="$timeBegin" --until="$timeEnd" --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { print loc }'`
+		dayTotal=$((dayTotal + dirTotal))
+	done;
+
+	# 日期 数量
+	echo "$dayBegin $dayTotal"
+done
+
